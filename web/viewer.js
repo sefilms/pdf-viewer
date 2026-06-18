ba2cf20a79aedad7e53e58074b8cf2ca56c4e329
sefilms <90103686+sefilms@users.noreply.github.com>
2026-06-18 18:27:26 -0400

    Update viewer.js (Cross-Origin Lock Error)

--- web/viewer.js
+++ web/viewer.js
@@ -1643,7 +1643,7 @@ exports.PDFViewerApplication = PDFViewerApplication;
       }
       const fileOrigin = new URL(file, window.location.href).origin;
       if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
+        // throw new Error("file origin does not match viewer's");
       }
     } catch (ex) {
       PDFViewerApplication.l10n.get("loading_error").then(msg => {
